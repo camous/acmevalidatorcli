1287ba197c9af3e2f467dff3d90c3fd80054d8c7
Fabien Camous <fabien.camous@bearingpoint.com>
2019-08-07 13:53:59 +0200

    add parameter to ignoreRequired fields

--- Program.cs
+++ Program.cs
@@ -9,14 +9,21 @@ namespace acmevalidatorcli
     class Program
     {
         static void Main(string[] args)
-        {
+        {           
             var inputjson = Encoding.UTF8.GetString(Convert.FromBase64String(args[0]));
             var input = JObject.Parse(inputjson);
 
             var rulesjson = Encoding.UTF8.GetString(Convert.FromBase64String(args[1]));
             var rules = JObject.Parse(rulesjson);
 
-            var valid = new acmevalidator.Validator().Validate(input, rules, out Dictionary<JToken, JToken> delta);
+            bool ignoreRequired = false;
+            if (args.Length == 3)
+            {
+                if(bool.TryParse(args[2], out ignoreRequired))
+                {}
+            }
+
+            var valid = new acmevalidator.Validator().Validate(input, rules, out Dictionary<JToken, JToken> delta, ignoreRequired);
             Console.WriteLine(valid);
 
             var arraydelta = new JArray();
