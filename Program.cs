0d6b219cd3e139ab29d8ecef5342b8501f1cfe8d
Fabien Camous <fabien.camous@bearingpoint.com>
2019-08-09 10:43:22 +0200

    better commandline support

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CommandLine;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -8,29 +9,37 @@ namespace acmevalidatorcli
 {
     class Program
     {
+        public class Options
+        {
+            [Option('i', "input", Required = true, HelpText ="input json in base64")]
+            public string input { get; set; }
+            [Option('r', "rules", Required = true, HelpText = "rules json in base64")]
+            public string rules { get; set; }
+
+            [Option('f', "ignorerequired", Required = false, HelpText = "ignore $required / $requiredOrNull", Default =false)]
+            public bool ignoreRequired { get; set; }
+        }
+
         static void Main(string[] args)
-        {           
-            var inputjson = Encoding.UTF8.GetString(Convert.FromBase64String(args[0]));
-            var input = JObject.Parse(inputjson);
+        {
+            Parser.Default.ParseArguments<Options>(args)
+               .WithParsed<Options>(o =>
+               {
+                   var inputjson = Encoding.UTF8.GetString(Convert.FromBase64String(o.input));
+                   var input = JObject.Parse(inputjson);
 
-            var rulesjson = Encoding.UTF8.GetString(Convert.FromBase64String(args[1]));
-            var rules = JObject.Parse(rulesjson);
+                   var rulesjson = Encoding.UTF8.GetString(Convert.FromBase64String(o.rules));
+                   var rules = JObject.Parse(rulesjson);
 
-            bool ignoreRequired = false;
-            if (args.Length == 3)
-            {
-                if(bool.TryParse(args[2], out ignoreRequired))
-                {}
-            }
+                   var valid = new acmevalidator.Validator().Validate(input, rules, out Dictionary<JToken, JToken> delta, o.ignoreRequired);
+                   Console.WriteLine(valid);
 
-            var valid = new acmevalidator.Validator().Validate(input, rules, out Dictionary<JToken, JToken> delta, ignoreRequired);
-            Console.WriteLine(valid);
+                   var arraydelta = new JArray();
+                   foreach (var d in delta)
+                       arraydelta.Add(new JObject { { "expected", d.Key }, { "actual", d.Value ?? null } });
 
-            var arraydelta = new JArray();
-            foreach (var d in delta)
-                arraydelta.Add(new JObject { { "expected", d.Key }, { "actual", d.Value ?? null } });
-
-            Console.WriteLine(Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(arraydelta))));
+                   Console.WriteLine(Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(arraydelta))));
+               });
         }
     }
 }
